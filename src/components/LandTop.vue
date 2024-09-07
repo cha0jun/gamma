b48619cbcc00cf8d4ea6af60691b151a645caa61
Matthew Tan <matthewcj.tan@gmail.com>
2024-09-07 13:38:24 +0800

    [chore] added LandTop

---
+++ src/components/LandTop.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+<div id="toplanding" class="flex justify-center p-40 gap-2"> 
+  <h1 class= "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl grow self-center">Find the right card for you.</h1>
+  <img id="cc" src="/src/assets/cc.jpg" class="rounded-xl grow cc">
+
+
+
+</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
