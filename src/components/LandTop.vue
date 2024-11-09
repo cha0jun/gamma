b5a7fb53e888f2d175567d32971b75aadee1e73b
Matthew Tan <matthewcj.tan@gmail.com>
2024-11-10 00:13:58 +0800

    [fix] cd

--- src/components/LandTop.vue
+++ src/components/LandTop.vue
@@ -5,7 +5,8 @@
 <template>
 <div id="toplanding" class="flex justify-center gap-x-5 header-width p-10">
   <div class="flex-1 mr-5 mt-5">
-    <h1 class= "scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-7xl">Find the right card for you. This is a CD test</h1>
+    <h1 class= "scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-7xl">Find the right card for you.</h1>
+    <h1>where u not heres</h1>
     <h2 class="lg:text-3xl font-medium mt-3">Gamma helps you compare benefits and find the card that fits your needs, easily.</h2>
   </div> 
   <img id="cc" src="/src/assets/cc.jpg" class="max-sm:hidden rounded-xl lg:grow cc">
